85e8f7ab9b563561fe01eeaef4b638d4a3ab50f3
eclonit <davizinhozl.mendes@gmail.com>
2018-08-13 22:44:41 -0300

    Atualização 1

---
+++ First_class/src/Class_Packge/First_Class_Project.java
@@ -0,0 +1,14 @@
+package Class_Packge;
+
+public class First_Class_Project {
+	public static void main(String args[]) {
+		
+		/* 
+		 * comentario */
+		
+		char A = 120;
+		
+		System.out.println(A);
+		
+	}
+}
