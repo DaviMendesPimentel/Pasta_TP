0b92136ac900235bc22daf2142b0a65099424045
eclonit <davizinhozl.mendes@gmail.com>
2018-08-20 22:51:25 -0300

    Atualização 2

--- First_class/src/Class_Packge/First_Class_Project.java
+++ First_class/src/Class_Packge/First_Class_Project.java
@@ -1,14 +1,19 @@
 package Class_Packge;
+import java.util.Scanner;
 
 public class First_Class_Project {
 	public static void main(String args[]) {
 		
-		/* 
-		 * comentario */
+		Scanner scan_in = new Scanner(System.in);
+		int first_number = 0, second_number = 0;
 		
-		char A = 120;
+		System.out.println("Digite um número");
+		first_number = scan_in.nextInt();
+		System.out.println("Digite outro número para\n" +
+							"A comparação ser executada");
+		second_number = scan_in.nextInt();
 		
-		System.out.println(A);
+		
 		
 	}
 }
