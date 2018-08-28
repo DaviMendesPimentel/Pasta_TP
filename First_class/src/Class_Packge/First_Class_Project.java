cb79a0f8ca76f483ea549e5e575ffbc703d1cecb
eclonit <davizinhozl.mendes@gmail.com>
2018-08-27 22:45:36 -0300

    Atualização 4

--- First_class/src/Class_Packge/First_Class_Project.java
+++ First_class/src/Class_Packge/First_Class_Project.java
@@ -2,18 +2,42 @@ package Class_Packge;
 import java.util.Scanner;
 
 public class First_Class_Project {
+	
+	private int number1, number2;	//cria duas variáveis de um objeto da classe First_Class_Project
+	
+	//Soma os inteiros recebidos
+	public int soma()	//declaração da função soma()
+	{
+		return number1 + number2;	//retorna a soma de number1 com number2
+	}
+	
+	//Subtrai os inteiros recebidos
+	public int sub()	//declaração da função sub()
+	{
+		return number2 - number1;	//retorna a subtração de number1 com number2
+	}
+	
+	//Mutiplica os inteiros recebidos
+	public int mult()	//declaração da função mult()
+	{
+		return number1 * number2;	//retorna a multiplicação de number1 com number2
+	}
+	
+	//Divide os decimais recebidos
+	public double div()	//declaração da função div()
+	{
+		return (double)(number1 / number2);	//retorna a divisão de number1 com number2(em double)
+	}
+	
+	
 	public static void main(String args[]) {
+		First_Class_Project FCPobj = new First_Class_Project();	//instancia um objeto de First_Class_Project
+		FCPobj.number1 = 6;	//inicializa number1 com 6
+		FCPobj.number2 = 6;	//incializa number2 com 10
 		
-		Scanner scan_in = new Scanner(System.in);
-		int first_number = 0, second_number = 0;
-		
-		System.out.println("Digite um número");
-		first_number = scan_in.nextInt();
-		System.out.println("Digite outro número para\n" +
-							"A comparação ser executada");
-		second_number = scan_in.nextInt();
-		
-		
-		
+		System.out.println(FCPobj.soma());	//imprime a soma
+		System.out.println(FCPobj.sub());	//imprime a subtração
+		System.out.println(FCPobj.mult());	//imprime a multiplicação
+		System.out.println(FCPobj.div());	//imprime a divisão
 	}
 }
